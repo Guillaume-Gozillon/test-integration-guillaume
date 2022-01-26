d577555bf2517b389879191b9e9593bd1d66d6ed
Guillaume <guillaumegozillon@gmail.com>
2022-01-26 15:09:10 +0100

    Add: dynamic content + CSS

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// console.log('hello');
+const items = document.querySelectorAll('.section3-container .text-section3')
+const nbItems = items.length
+
+const prev = document.querySelector('.left-arrow')
+const next = document.querySelector('.right-arrow')
+let count = 0
+
+const nextSlide = () => {
+  items[count].classList.remove('active')
+
+  if (count < nbItems - 1) {
+    count++
+  } else {
+    count = 0
+  }
+  items[count].classList.add('active')
+}
+
+const prevSlide = () => {
+  items[count].classList.remove('active')
+
+  if (count > 0) {
+    count--
+  } else {
+    count = nbItems - 1
+  }
+  items[count].classList.add('active')
+}
+
+next.addEventListener('click', nextSlide)
+prev.addEventListener('click', prevSlide)
